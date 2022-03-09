307ea405a2bfe0a64ec1292d58c79d23aef7ac97
Lyra Gordon <lyragordon@asu.edu>
2022-03-08 16:41:02 -0800

    Dependencies

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,13 @@
 #include <Arduino.h>
+#include <AccelStepper.h>
+#include <Adafruit_MLX90640.h>
+
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+  Serial.println("~~~~~~ SERIAL INITIATED ~~~~~~");
+  Serial.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 }
 
 void loop() {
