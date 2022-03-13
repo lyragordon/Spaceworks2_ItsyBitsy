331ffbaae57ddea600502296ce3afe8264757fb9
Lyra Gordon <lyragordon@asu.edu>
2022-03-13 12:10:36 -0700

    Added MLX example code from adafruit

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include <AccelStepper.h>
 #include <Adafruit_MLX90640.h>
+#include <Adafruit_BusIO_Register.h>
 
 // ~~~~~ Pin Definitions ~~~~~
 // Stepper motor
@@ -21,6 +22,8 @@ int outputPins[] = {OPTO_EN, SLP};
 
 AccelStepper stepper = AccelStepper(AccelStepper::FULL4WIRE, A1, A2, B1, B2);
 Adafruit_MLX90640 cam;
+
+float frame[32 * 24]; // buffer for full frame of temperatures
 
 // ~~~~~ Support Functions ~~~~~
 
@@ -65,9 +68,94 @@ void setup()
   {
     Serial.println("!!!!!Stepper homing failed!!!!!!");
   }
+
+  // stolen from example
+  Serial.print("Serial number: ");
+  Serial.print(cam.serialNumber[0], HEX);
+  Serial.print(cam.serialNumber[1], HEX);
+  Serial.println(cam.serialNumber[2], HEX);
+
+  // cam.setMode(MLX90640_INTERLEAVED);
+  cam.setMode(MLX90640_CHESS);
+  Serial.print("Current mode: ");
+  if (cam.getMode() == MLX90640_CHESS)
+  {
+    Serial.println("Chess");
+  }
+  else
+  {
+    Serial.println("Interleave");
+  }
+
+  cam.setResolution(MLX90640_ADC_18BIT);
+  Serial.print("Current resolution: ");
+  mlx90640_resolution_t res = cam.getResolution();
+  switch (res)
+  {
+  case MLX90640_ADC_16BIT:
+    Serial.println("16 bit");
+    break;
+  case MLX90640_ADC_17BIT:
+    Serial.println("17 bit");
+    break;
+  case MLX90640_ADC_18BIT:
+    Serial.println("18 bit");
+    break;
+  case MLX90640_ADC_19BIT:
+    Serial.println("19 bit");
+    break;
+  }
+
+  cam.setRefreshRate(MLX90640_2_HZ);
+  Serial.print("Current frame rate: ");
+  mlx90640_refreshrate_t rate = cam.getRefreshRate();
+  switch (rate)
+  {
+  case MLX90640_0_5_HZ:
+    Serial.println("0.5 Hz");
+    break;
+  case MLX90640_1_HZ:
+    Serial.println("1 Hz");
+    break;
+  case MLX90640_2_HZ:
+    Serial.println("2 Hz");
+    break;
+  case MLX90640_4_HZ:
+    Serial.println("4 Hz");
+    break;
+  case MLX90640_8_HZ:
+    Serial.println("8 Hz");
+    break;
+  case MLX90640_16_HZ:
+    Serial.println("16 Hz");
+    break;
+  case MLX90640_32_HZ:
+    Serial.println("32 Hz");
+    break;
+  case MLX90640_64_HZ:
+    Serial.println("64 Hz");
+    break;
+  }
 }
 
 void loop()
 {
-  // put your main code here, to run repeatedly:
+  // more stolen code
+  delay(500);
+  if (cam.getFrame(frame) != 0)
+  {
+    Serial.println("Failed");
+    return;
+  }
+  Serial.println();
+  Serial.println();
+  for (uint8_t h = 0; h < 24; h++)
+  {
+    for (uint8_t w = 0; w < 32; w++)
+    {
+      float t = frame[h * 32 + w];
+      Serial.print(t, 1);
+      Serial.print(", ");
+    }
+  }
 }
